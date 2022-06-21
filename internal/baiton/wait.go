c770f597851877e87ed2b6d9aa4c67bcc0144d6a
Artalus <>
2022-06-21 21:41:10 +0300

    primitive wait for file

--- internal/baiton/wait.go
+++ internal/baiton/wait.go
@@ -1,5 +1,23 @@
 package wait
 
-func Add(x, y int) int {
-	return x + y
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
+
+func WaitFile(filepath string, delay_s, timeout_s int) error {
+	timeout := time.Now().Add(time.Duration(timeout_s) * time.Second)
+	for {
+		if _, err := os.Stat(filepath); errors.Is(err, os.ErrNotExist) {
+			sleep := time.Duration(delay_s) * time.Second
+			time.Sleep(sleep)
+		} else {
+			return nil
+		}
+		if time.Now().After(timeout) {
+			return fmt.Errorf("file %v did not appear in %v seconds", filepath, timeout_s)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -3,13 +3,48 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+
 	bt "internal/baiton"
 )
 
-func main() {
-	num := flag.Int("n", 1050, "number")
-	flag.Parse()
-	r := bt.Add(*num, *num)
+var filepath = flag.String("file", "", "Wait for this file")
+var delay = flag.Int("delay", 1, "Wait for D seconds between checks")
+var timeout = flag.Int("timeout", 60, "Fail if file did not appear in T seconds")
 
-	fmt.Printf("Hello, %v\n", r)
+func checkArgs() bool {
+	if *filepath == "" {
+		fmt.Println("ERROR: file not specified")
+		return false
+	}
+	if *delay < 0 {
+		fmt.Println("ERROR: delay should be >0")
+		return false
+	}
+	if *timeout < 0 {
+		fmt.Println("ERROR: timeout should be >0")
+		return false
+	}
+	return true
+}
+
+func main() {
+	flag.Parse()
+	checkArgs()
+
+	if !checkArgs() {
+		os.Exit(1)
+	}
+
+	file := *filepath
+	d := *delay
+	t := *timeout
+
+	result := bt.WaitFile(file, d, t)
+	if result != nil {
+		fmt.Printf("ERROR: %v\n", result)
+		os.Exit(2)
+	}
+
+	fmt.Printf("file %v is ready\n", file)
 }
